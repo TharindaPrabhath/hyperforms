ada1ee8364ce065a34421c3011bec5a8e8863a54
TharindaPrabhath <tharindahp@gmail.com>
2024-10-03 20:45:46 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from '@/components/ui/button';
+
+export default function Home() {
+  return (
+    <div>
+      Welcome to HyperForms
+      <Button>Click</Button>
+    </div>
+  );
+}
